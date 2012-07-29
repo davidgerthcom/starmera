5df07c1d69836673d275da38aa6fcb0100fc413b
David Gerth <git@davidgerth.com>
2012-07-30 00:22:36 +0200

    gitignore and deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,5 @@
 echo "Removing current mnemonic directory..."
-ssh david@davidgerth.com rm -rf /var/www/portfolio
+ssh david@davidgerth.com rm -rf /var/www/starmera
 
 echo "Cloning git repository into davidgerth.com/var/www/portfolio..."
-ssh david@davidgerth.com git clone https://github.com/davidgerthcom/portfolio.git /var/www/portfolio
+ssh david@davidgerth.com git clone https://github.com/davidgerthcom/starmera.git /var/www/starmera
